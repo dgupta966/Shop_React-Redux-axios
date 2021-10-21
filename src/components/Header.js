e749e9b0cfadedbe53c38dc563a923df1280ac7a
Deepak Gupta <88889634+dgupta966@users.noreply.github.com>
2021-10-21 16:29:43 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Shop</h2>
+      </div>
+        <h4 style={{marginRight:"0"}}>@deepakgupta</h4>
+    </div>
+  );
+};
+
+export default Header;
